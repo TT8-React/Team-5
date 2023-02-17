d223496220dbca157808b070febb48e3cf6d789d
MRayyan <eng.m.rayyan@gmail.com>
2023-02-17 21:20:43 +0200

    feat: move data form component to mock

--- src/pages/CompanyDetails/index.jsx
+++ src/pages/CompanyDetails/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+// import  local Styled Components
 import {
   StyledCompanyDetailsPageBody,
   StyledDetailInfo,
@@ -6,22 +7,21 @@ import {
   StyledDetailsBox,
   StyledDetailsPage,
 } from "./StyledCompanyDetails";
+
+// import  Global Styled Components
 import {
   DashCard,
   MainButton,
   StyledButtonBox,
 } from "../../styled/styleComponent";
+
+// import mock
+import { details } from "../../mock/companyDetails";
+
+// import the Layout of pages
 import Layout from "../../components/Layout/Layout";
+
 function CompanyDetails() {
-  const details = [
-    { id: 0, detailName: "name", detailInfo: "Lorem ipsum" },
-    { id: 1, detailName: "test 1", detailInfo: "Lorem ipsum" },
-    { id: 2, detailName: "test 2", detailInfo: "Lorem ipsum" },
-    { id: 3, detailName: "test 3", detailInfo: "Lorem ipsum" },
-    { id: 4, detailName: "test 4", detailInfo: "Lorem ipsum" },
-    { id: 5, detailName: "test 5", detailInfo: "Lorem ipsum" },
-    { id: 6, detailName: "test 6", detailInfo: "Lorem ipsum" },
-  ];
   return (
     <Layout title="Company details">
       <StyledCompanyDetailsPageBody>
