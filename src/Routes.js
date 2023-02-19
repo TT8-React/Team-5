ed326a0c5f72d39fd78a4b0f279dc0b74280c4f7
MRayyan <eng.m.rayyan@gmail.com>
2023-02-19 18:15:40 +0200

    create page 9 and page 16

--- src/Routes.js
+++ src/Routes.js
@@ -1,25 +1,31 @@
-import { useContext } from "react"
-import { Route, Routes, Navigate } from "react-router-dom"
-import PageNotFound from "./components/PageNotFound"
-import { AuthContext } from "./context"
-import SignUp from "./pages/SignUp"
-import LogIn from "./pages/LogIn"
-import CompanyDetails from "./pages/CompanyDetails"
-import PrivateRoute from "./PriveteRoutes"
+import { useContext } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import PageNotFound from "./components/PageNotFound";
+import { AuthContext } from "./context";
+import SignUp from "./pages/SignUp";
+import LogIn from "./pages/LogIn";
+import CompanyDetails from "./pages/CompanyDetails";
+import PrivateRoute from "./PriveteRoutes";
 
-const MyRoutes = ()=>{
-    const [ isAuthenticated,  ] = useContext(AuthContext);
-    return (
-        <Routes>
-          <Route index element={<Navigate to='/Login' />} />
-          <Route path="/Login" element={isAuthenticated?<Navigate to='/Home' />:<LogIn/>} />
-          <Route path="/Signup" element={isAuthenticated?<Navigate to='/Home' />:<SignUp />} />
-          <Route path="/Home" element={<PrivateRoute/>}>
-            <Route index element={<CompanyDetails/>} />
-            <Route path="*" element={<PageNotFound/>} />
-          </Route>
-            <Route path="/*" element={<PageNotFound/>} />
-        </Routes>
-    )
-}
+const MyRoutes = () => {
+  const [isAuthenticated] = useContext(AuthContext);
+  return (
+    <Routes>
+      <Route index element={<Navigate to="/Login" />} />
+      <Route
+        path="/Login"
+        element={isAuthenticated ? <Navigate to="/Home" /> : <LogIn />}
+      />
+      <Route
+        path="/Signup"
+        element={isAuthenticated ? <Navigate to="/Home" /> : <SignUp />}
+      />
+      <Route path="/Home" element={<PrivateRoute />}>
+        <Route index element={<CompanyDetails />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Route>
+      <Route path="/*" element={<PageNotFound />} />
+    </Routes>
+  );
+};
 export default MyRoutes;
---
+++ src/pages/ProfileDetails/index.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Input from "../../components/InputBox";
+import Layout from "../../components/Layout/Layout";
+import { MainButton, StyledButtonBox } from "../../styled/styleComponent";
+import { StyledForm, StyledInputsContainer, StyledSection } from "./Styled";
+
+import { profileDetails } from "../../mock/profileDetails";
+
+function ProfileDetails() {
+  return (
+    <Layout title="Profile details">
+      <StyledSection>
+        <StyledButtonBox>
+          <MainButton>View details</MainButton>
+        </StyledButtonBox>
+        <StyledForm>
+          <StyledInputsContainer>
+            {profileDetails.map((item) => {
+              return (
+                <Input
+                  key={item.key}
+                  id={item.id}
+                  label={item.label}
+                  type={item.type}
+                  placeholder={item.placeholder}
+                />
+              );
+            })}
+          </StyledInputsContainer>
+          <MainButton type="submit" className="mt-100">
+            Submit
+          </MainButton>
+        </StyledForm>
+      </StyledSection>
+    </Layout>
+  );
+}
+
+export default ProfileDetails;
